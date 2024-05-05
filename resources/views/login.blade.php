0838f3abf917ce8739fd2db718ff60c96878f417
rey4L <reynardetwaroo4@gmail.com>
2024-05-05 19:50:49 -0400

    Add login form styling.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,25 @@
-<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
-@vite(['resources/js/app.js'])
-<div>
-        
-    <h1 class="omega">Hello User!</h1>
-    <h2>Lorem ipsum</h2>
-</div>
+@vite(['resources/css/login/login_form.css'])
 
+<div class="form-container">
 <form method="POST" action=/login>
     @csrf
+    
+    <h1>Login</h1>
+    
     <div>
         <label for ="email">Email</label>
-        <input id="email" type="email" name="email" required autofocus>
+        <input id="email" type="email" name="email" placeholder="user@example.com" required autofocus>
     </div>
     <div>
         <label for="password">Password</label>
-        <input type="password" name="password" id="password" required autofocus>
+        <input type="password" name="password" id="password" placeholder="password" required autofocus>
     </div>
     <div>
-        <button type="submit">Login</button>
+        <button type="submit">LOGIN</button>
     </div>
+    <div class="register-link">
+    Don't have an account? <a href="/register">Register</a>
+</div>
 </form> 
+</div>
+
