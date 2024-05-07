4540af0d9cef98536867b1c11c36d2071e2e2b12
shivmoh <shiveshmohamed1234@gmail.com>
2024-05-07 11:56:07 -0400

    something something about something styling and what not

--- resources/views/supplier/request-form.blade.php
+++ resources/views/supplier/request-form.blade.php
@@ -4,19 +4,39 @@
 <form id="regForm" method="POST" action="/review">
     @csrf
         <div class="row">
-            <select name="item" id="item">
-                @foreach ($result[0]['items'] as $item)
-                    <option value={{$item->id}} selected>{{$item->name}}</option>            
-                @endforeach
-            </select>
-            <p><input placeholder="Amount" type="number" name="amount" id="amount"></p>
+            <div class="title-container">
+                <h1 class="section-title">Select Items</h1>
+            </div>
+            <div class="item-container">
+
+                <div class="input-container">
+
+                    <select name="item" id="item">
+                        @foreach ($result[0]['items'] as $item)
+                            @php
+                                $item_name = str_replace(" ", "_", $item->name);
+                                $item_id = $item->id;
+                                $item_value = $item_name."x".$item_id;
+                            @endphp
+                            <option value={{$item_value}}>{{$item->name}}</option>            
+                        @endforeach
+                    </select>
+                    <p><input placeholder="Amount" type="number" name="amount" id="amount"></p>
+                </div>
+                <button type="button" onclick="addItem()">Add Item</button>             
+            </div>
+
             <p class="item-display" id="item-display"></p>
+            <p class="item-displayer" id="item-displayer"></p>
+
             <input type="hidden" name="items" id="items">
             <input type="hidden" name="supplier" id="supplier" value={{$result[0]['supplier']->id}}>
-            <button type="button" onclick="addItem()">Add Item</button>         
         </div>
         
         <div class="row">
+            <div class="title-container">
+                <h1 class="section-title">Select Company Card</h1>
+            </div>
             <select name="payment" id="payment">
                 @foreach ($result[0]['cards'] as $card)
                     <option value={{$card->id}} selected>{{$card->card_holder}}</option>            
@@ -24,7 +44,9 @@
             </select>
         </div>
 
-        <button type="submit">Submit</button>
+        <div class="button-container">
+            <button type="submit" class="order-item">Submit</button>
+        </div>
             
     </div>
 </form>
--- resources/views/supplier/supplier.blade.php
+++ resources/views/supplier/supplier.blade.php
@@ -1,22 +1,39 @@
 @vite(['resources/js/supplier/request_form.js'])
 @vite(['resources/css/supplier/supplier.css'])
 
-{{$error}}
 
 <section class="filter-section">
     <div class="filter-container">
-        <form  action="/supplier" method="POST" class="search-bar">
-            @csrf
-            <input 
-                type="search" 
-                name="search" 
-                id="search-bar"
-                placeholder="Search by name"
-                >
-            <button type="submit">Search</button>
-        </form>
+        <div class="search-container">
+            <form  action="/supplier" method="POST" class="search-bar" id="search">
+                @csrf
+                <input 
+                    type="search" 
+                    name="search" 
+                    id="search-bar"
+                    placeholder="Search by name"
+                    >
+            </form>
+            <form  action="/supplier" method="POST" id="clear">
+                @csrf
+            </form>
+           
+            <div class="button-container">
+                <button type="submit" form="search" class="search-button">Search</button>
+                <button type="submit" form="clear" class="clear-button">Clear</button>
+            </div>
+        </div>
+        
     </div>
 </section>
+
+
+@if ($error)
+    <section class="errors">
+        <h1>No results found</h1>
+        <h3 class="suggestion">Perhaps try using a different term!</h3>
+    </section>
+@endif
 
 <section class="suppliers">
     @foreach ($suppliers as $supplier )
@@ -29,7 +46,6 @@
         </div>
 
         <div class="info-section">
-            <p>{{$supplier['supplier']->id}}</p>
             <h3 class="name">{{$supplier['supplier']->name}}</h3>
             <h4 class="description">{{$supplier['supplier']->description}}</h2>
             <div class="contacts">
@@ -37,11 +53,9 @@
                 <h4 class="numbers"><span class="label">Call at: </span>{{$supplier['contact']->primary_number}} OR {{$supplier['contact']->secondary_number}}</h4>                    
             </div>
             <input type="hidden" name="id" value={{$supplier['supplier']->id}}>
-            <button type="submit">Order Items</button>
+            <button type="submit" class="order-item-button">Order Items</button>
         </div>
     </form>
-
-    
 
     @endforeach
 
